bee19c20e249c029f28f8f8e6fcf8cfcc47ea21d
alexjunyuexuli <140064725+alexjunyuexuli@users.noreply.github.com>
2023-11-04 12:08:42 +0000

    update

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,8 +3,9 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    call_me();
+    let a:i32 =3;
+    call_me(a);
 }
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -8,6 +8,6 @@
 fn main() {
     let x :i32= 3;
     println!("Number {}", x);
-    x = 5; // don't change this line
+    let x:i32 = 5; // don't change this line
     println!("Number {}", x);
 }
